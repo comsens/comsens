598fdee1721e96913064b9147ba313d86f11c3a7
comsens <nungju@gmail.com>
2017-01-05 14:42:56 +0900

    Update grabEbookFromPacktTask.bat

--- grabEbookFromPacktTask.bat
+++ grabEbookFromPacktTask.bat
@@ -4,11 +4,3 @@ rem If you want to log downloading history, just remove rem
 rem echo ***Date: %DATE:/=-% [%TIME::=:%] *** >> grabPacktFreeBook.log
 rem python grabPacktFreeBook.py >> grabPacktFreeBook.log 2>&1
 rem echo:
-rem echo:>> grabPacktFreeBook.log
-python grabPacktFreeBook.py 
-echo *** Done ! ***
-pauseaaaaaaaaaaaaaaaaaaaaaasaaaaaaas
-sdf
-sdf
-sdf
-sdf
